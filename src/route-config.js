829c6f85989e26c1bdbd616a9c5a3a4c815f1beb
Me1kro <191926083@qq.com>
2016-12-01 13:28:20 +0800

    vue start

---
+++ src/route-config.js
@@ -0,0 +1,18 @@
+ import Home from './components/Home.vue'
+ import About from './components/About.vue'
+
+export default [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    // {
+    //     'home': {
+    //         component: Home
+    //     },
+    //     'about': {
+    //         component: About
+    //     }
+    // }
+]
