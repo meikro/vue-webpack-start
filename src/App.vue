9926b7617a03d56c86b4bf8a8562d41d0e1d29ce
Me1kro <191926083@qq.com>
2016-12-01 17:38:00 +0800

    ax

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-	<div id="app">
-		<div class="row">
-			<div class="col-xs-offset-2 col-xs-8">
-				<div class="page-header">
-					<h2>Vue Start</h2>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-2 col-xs-offset-2">
-				<div class="list-group">
-					<!--使用指令v-link进行导航-->
-					<a class="list-group-item" v-link="{ path: '/Ajax'}">Ajax</a>
-					<a class="list-group-item" v-link="{ path: '/about'}">About</a>
-				</div>
-			</div>
-			<div class="col-xs-6">
-				<div class="panel">
-					<div class="panel-body">
-						<!--用于渲染匹配的组件-->
-						<router-view></router-view>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
 </template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import routeConfig from './route-config'
+import App from './App'
 
-
-Vue.use(VueRouter)
-Vue.use(VueResource)
-
-// create router
-const router = new VueRouter()
-router.map(routeConfig)
-const App = Vue.extend(require('./app.vue'))
-router.start(App, '#app')
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
